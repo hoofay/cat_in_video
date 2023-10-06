71a2f1ecc8de73066f7fbbe41dc829a96ad32080
Daniel Hough <32570623+hoofay@users.noreply.github.com>
2023-10-06 16:35:04 +0100

    Update main.py annotation

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import pandas as pd
 chosen_model = "yolo-v5.tflite"
 chosen_source_directory = 'test_videos_to_process'
 chosen_output_directory = 'processed_videos'
-chosen_frame_breaks = 10000 
+chosen_frame_breaks = 600 # roughly 10s of video
 d = {'class': [15, 77],'class_name': ['cat','teddy bear'] , 'threshold': [0.5, 0.5]} # see list here: https://github.com/ultralytics/yolov5/blob/master/data/coco128.yaml
 my_criteria = pd.DataFrame(data=d)
 
